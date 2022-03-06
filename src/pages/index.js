df93e1d72615c0881d912101d5f67514a62c2e49
HananeMiloud <hananemiloud37@gmail.com>
2022-03-06 16:47:46 +0100

    add page9

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 export { default as Page1 } from './stagiaire/page1/Page1';
 export { default as Page4 } from './stagiaire/page4/Page4';
-
+export { default as Page9 } from './stagiaire/page9/Page9';
 export { default as Page12 } from './entreprise/page12/Page12';
 export { default as Page13 } from './entreprise/page13/Page13';
 export { default as Page0 } from './home-page/Page0';
